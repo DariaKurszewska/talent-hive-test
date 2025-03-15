5983cb8444e3722279baf7b16e87d38b9dafbdc0
Daria <daria.testuj.pl@gmail.com>
2025-03-15 13:05:01 +0100

    CreateCandidatesTest.java | Add logging for request data and candidate ID

--- src/test/java/apitest/CreateCandidatesTest.java
+++ src/test/java/apitest/CreateCandidatesTest.java
@@ -2,6 +2,7 @@ package apitest;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import model.Candidate;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
+@Slf4j
 public class CreateCandidatesTest {
 
     private static final String BASE_URL = "http://localhost:8080/api/v1/candidates";
@@ -27,6 +29,8 @@ public class CreateCandidatesTest {
     public void testCreateCandidateAndExtractId() {
 
         Candidate candidate = CreateCandidateData.generateCandidate();
+
+        log.info("Creating candidate with name: {}", candidate.getName());
 
         Response response = given()
                 .contentType("application/json")
@@ -48,7 +52,7 @@ public class CreateCandidatesTest {
 
         candidateId = response.path("id");
 
-        System.out.println("New candidate created with ID: " + candidateId);
+        log.info("New candidate created with ID: {}", candidateId);
     }
 
     @AfterAll
