126f54640a490a0cb01b6e0f2da9a472dd4337ef
Daria <daria.testuj.pl@gmail.com>
2025-03-19 11:35:12 +0100

    CreateCandidateData.java | Implemented generated data for status and interview date.

--- src/main/java/model/CreateCandidateData.java
+++ src/main/java/model/CreateCandidateData.java
@@ -2,19 +2,45 @@ package model;
 
 import com.github.javafaker.Faker;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 public class CreateCandidateData {
 
     private static final Faker faker = new Faker();
+    private static final Random random = new Random();
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
 
     public static Candidate generateCandidate() {
         Candidate candidate = new Candidate();
         candidate.setName(faker.name().fullName());
         candidate.setEmail(faker.internet().emailAddress());
         candidate.setPositionApplied(faker.job().title());
-        candidate.setStatus("Interview Scheduled");
-        candidate.setInterviewDate("2025-12-31T10:00:00.000+00:00");
+
+        String status = generateStatus();
+        candidate.setStatus(status);
+
+        candidate.setInterviewDate(generateFutureDate());
+
         candidate.setRecruiter(faker.name().fullName());
 
         return candidate;
+    }
+
+    private static String generateStatus() {
+        return random.nextBoolean() ? "Interview Scheduled" : "Not Scheduled";
+    }
+
+    private static String generateFutureDate() {
+        ZonedDateTime futureDate = ZonedDateTime.now(ZoneId.of("UTC")).plusDays(random.nextInt(22) + 7);
+
+        // Formatting the date and manually appending the "+00:00" timezone offset
+        // This is done manually to ensure the time zone is set as "+00:00" instead of "Z"
+        // which could lead to a defect or mismatch when comparing with expected results.
+        return futureDate.format(formatter) + "+00:00";
+
     }
 }
